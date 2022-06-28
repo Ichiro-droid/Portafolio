87aa572c130987108d297d9c63ebae4dfd2d3886
イチロー <83840811+Ichiro-droid@users.noreply.github.com>
2022-06-28 15:18:25 -0300

    1st commit

--- contacto.php
+++ contacto.php
@@ -1,13 +1,22 @@
-<?php 
-	$nombre = $_POST['name'];
-	$asunto = $_POST['subject'];
-	$email = $_POST['email'];
-	$mensaje = $_POST['message'];
+<?php  
 
-	echo $nombre. "ha dicho <br/>".$mensaje;
-	if(mail('ka8959342@gmail.com', $asunto, $mensaje,$email)){
-		echo "mail enviado";
-	}else{
-		echo "uyuyuyuyuy";
-	}
- ?>
+// Llamando a los campos
+$name = $_POST['name'];
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = $_POST['menmessagesaje'];
+
+// Datos para el correo
+$destinatario = "codytron.info@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
+
+?>
