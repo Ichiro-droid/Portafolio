bfb8ae3bea6dc406d3ff64f403b1238ff30fdf93
イチロー <83840811+Ichiro-droid@users.noreply.github.com>
2022-06-28 15:38:15 -0300

    1st commit

--- contacto.php
+++ contacto.php
@@ -1,24 +1,26 @@
 <?php  
 
-	// Llamando a los campos
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$asunto = $_POST['subject'];
-	$mensaje = $_POST['message'];
-	
-	// Datos para el correo
-	$destinatario = "Ka8959342@gmail.com";
-	$asunto1 = "Contacto desde nuestra web";
-	
-	$carta = "De: $name \n";
-	$carta .= "Correo: $email \n";
-	$carta .= "Asunto: $asunto \n";
-	$carta .= "Mensaje: $mensaje";
-	
-	// Enviando Mensaje
-	if(mail($destinatario, $asunto1, $carta)){
-		echo "mail enviado";
-	}else{
-		echo "uyuyuyuyuy";
-	}
+// Llamando a los campos
+$name = $_POST['name'];
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = $_POST['message'];
+
+// Datos para el correo
+$destinatario = "Ka8959342@gmail.com";
+$asunto1 = "Contacto desde nuestra web";
+
+$carta = "De: $name \n";
+$carta .= "Correo: $email \n";
+$carta .= "Asunto: $asunto \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto1, $carta)
+header('Location:index.html')
+/*if(mail($destinatario, $asunto1, $carta)){
+	echo "mail enviado";
+}else{
+	echo "uyuyuyuyuy";
+}*/
 ?>
