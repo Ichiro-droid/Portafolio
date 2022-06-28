f82c59c86d4c19a908d4ebf854fd820590ce1ebd
イチロー <83840811+Ichiro-droid@users.noreply.github.com>
2022-06-28 15:33:36 -0300

    1st commit

--- contacto.php
+++ contacto.php
@@ -1,21 +1,24 @@
 <?php  
 
-// Llamando a los campos
-$name = $_POST['name'];
-$email = $_POST['email'];
-$asunto = $_POST['subject'];
-$mensaje = $_POST['message'];
-
-// Datos para el correo
-$destinatario = "Ka8959342@gmail.com";
-$asunto1 = "Contacto desde nuestra web";
-
-$carta = "De: $name \n";
-$carta .= "Correo: $email \n";
-$carta .= "Asunto: $asunto \n";
-$carta .= "Mensaje: $mensaje";
-
-// Enviando Mensaje
-mail($destinatario, $asunto1, $carta);
-
+	// Llamando a los campos
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$asunto = $_POST['subject'];
+	$mensaje = $_POST['message'];
+	
+	// Datos para el correo
+	$destinatario = "Ka8959342@gmail.com";
+	$asunto1 = "Contacto desde nuestra web";
+	
+	$carta = "De: $name \n";
+	$carta .= "Correo: $email \n";
+	$carta .= "Asunto: $asunto \n";
+	$carta .= "Mensaje: $mensaje";
+	
+	// Enviando Mensaje
+	if(mail($destinatario, $asunto1, $carta)){
+		echo "mail enviado";
+	}else{
+		echo "uyuyuyuyuy";
+	}
 ?>
